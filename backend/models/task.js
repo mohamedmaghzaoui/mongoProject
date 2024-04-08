5b521668f8d42d129e20f82f6fc5565a56280a32
ramlaa2544 <106739799+ramlaa2544@users.noreply.github.com>
2024-04-08 16:52:21 +0200

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -1,25 +1,30 @@
 const mongoose=require("mongoose")
 const TaskSchema=new mongoose.Schema({
+    //task title
     title:{
         type:String,
         required:true,
         
         
     },
+    //task date 
     date:{
         type:Date,
         required:true,
     },
+    //task description
     description:{
         type:String,
         required:false
     },
+    //task username
     username:{
         type:String,
         required:true,
         index:true
     },
-   
+    
+   //reference
     parentTask: {
         type: mongoose.Schema.Types.ObjectId, // This field holds the ObjectId of the referenced task
         ref: 'Task' // Reference to the same schema
