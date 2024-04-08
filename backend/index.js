dec055f82fc29dd5b9f1720a2fe00563411ad4a8
ramlaa2544 <106739799+ramlaa2544@users.noreply.github.com>
2024-04-08 16:58:17 +0200

    modified backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+//config env variables
 require('dotenv').config()
+//get database url from env varaiable for security
 const databaseUrl=process.env.DATABASE_URL
+//api uri
 const taskRouter = require("./routes/task");
 
 const app = express();
@@ -10,7 +13,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/tasks", taskRouter); 
+//get port using env variable for hosting site on render
 const Port=process.env.PORT
+//connect to mongo db database  and run server
 mongoose.connect(databaseUrl)
     .then(() => {
         app.listen(Port|| 3001, () => {
